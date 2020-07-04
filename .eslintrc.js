96b3d7bc3cca390bf8c726deb76dcf9fbd00673d
Emilio Bertò <emilio.berto1@gmail.com>
2020-07-04 15:22:36 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2020: true,
+		node: true,
+	},
+	extends: [
+		'airbnb-base',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
+		'prettier/@typescript-eslint',
+		'plugin:prettier/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 11,
+		sourceType: 'module',
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		'no-console': 'off',
+		'import/extensions': [
+			'error',
+			'ignorePackages',
+			{
+				js: 'never',
+				jsx: 'never',
+				ts: 'never',
+				tsx: 'never',
+			},
+		],
+		'prettier/prettier': 'error',
+		'@typescript-eslint/explicit-function-return-type': ['error'],
+	},
+};
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+const a = 4;
+
+console.log(a);
+
+function funzioncina(): number {
+	const ddd = 2;
+
+	return ddd;
+}
+
+export default funzioncina;
